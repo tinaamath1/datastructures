8fcde8af6ce767f20222b6b6b1997fee5e60698b
tinaamath1 <93090945+tinaamath1@users.noreply.github.com>
2022-11-29 15:22:35 -0500

    Update aa02.js

--- aa02.js
+++ aa02.js
@@ -8,16 +8,16 @@ var allMeetings =[]
 
 // load the cheerio object into a variable, `content`
 // which holds data and metadata about the html file (written as txt)
-var content = fs.readFileSync('data/m08.txt');
+var content = fs.readFileSync('/home/ec2-user/environment/data/m08.txt');
 
 // load `content` into a cheerio object
 var $ = cheerio.load(content);
 
 $('tr').each(function(i, elem) {
     if ($(elem).attr("style")=="margin-bottom:10px") {
-        let row = $(elem).html();
-        parseZones(row);
-        console.log('*************')
+        allMeetings.push(parseZones($(elem).html()));
+        // parseZones(row);
+        // console.log('*************')
         
 
                 // var thisMeeting = {}; // Your function and data collection go here! 
@@ -28,65 +28,55 @@ $('tr').each(function(i, elem) {
 
     // create a function 
 
-    function parseZones (rawText) {
+    function parseZones (meetingInfo) {
         // create an object to hold locationName, addressName, address, directionInstructions, state, and zipCode
         
           // get metadata
-        const meetings = rawText.split("</td>")[0]
-        console.log (meetings)
-    //     // const meetingDays_arr = rawText.split('\t\t\t\t  \t    <b>').slice(2)
-    //     // console.log (meetingDays_arr) 
-        
-        
-    //     // const locationDetails = {}
-    //     // locationDetails.locationName = rawText.split('h4 style="margin:0;padding:0')[1].split('</h4><br>')[0].split(';">')[1]
-    //     // locationDetails.addressName = rawText.split('\t\t\t\t  \t    <b>')[1].split('</b><br>\n')[0]
-    //     // locationDetails.address = rawText.split('\t\t\t\t\t\t')[1].split(', \n')[0];
-    //     // // locationDetails.directionInstructions= rawText.split('<b>')[1].split("<br>")[2].replace("\t\t\t\t\t\t",'').trim()//.split('\t\t\t\t\t\t<br>(')[1]//.split('\n')[0]
-    //     // // locationDetails.state= 'NY';
-    //     // locationDetails.zipCode= rawText.match(/(\d{5})/g);//.split(" NY ")[1].slice(0,5)
-    //     // locationDetails.wheelChairAccess= rawText.includes('Wheelchair Access');
-    //     // locationDetails.meetings=[]
-    //     // console.log(locationDetails)
+        // const meetings = rawText.split("</td>")[0]
+        // console.log (meetings)
+  
         
     
-    //     const meetingDays_arr = rawText.split("</td>")[1].split('\t\t\t\t  \t    <b>').splice(1)
-    //     console.log (meetingDays_arr)  
-    //     // meetings = []
-
-    // //meeting times 
+        let aa_locationName = meetingInfo.split('h4 style="margin:0;padding:0')[1].split('</h4><br>')[0].split(';">')[1]
+        let aa_addressName = meetingInfo.split('\t\t\t\t  \t    <b>')[1].split('</b><br>\n')[0]
+        let aa_address = meetingInfo.split('\t\t\t\t\t\t')[1].split(', \n')[0]
+        //directionInstructions= meetingInfo.split('<b>')[1].split("<br>")[2].replace("\t\t\t\t\t\t",'').trim()//.split('\t\t\t\t\t\t<br>(')[1]//.split('\n')[0]
+        let state = 'NY';
+        // let aa_zipCode= meetingInfo.match(/\d{5}\)
+        // .split(" NY ")[1].slice(0,5)
+        let aa_wheelChairAccess= meetingInfo.includes('Wheelchair Access')
+       
         
-    //      for (let i = 1; i < meetingDays_arr.length; i ++ ) {
+        let meetingDays = meetingInfo.split('\t\t\t\t  \t    <b>').slice(2)
+        let meetingsList=[]
         
-    //     let newmeeting = meetingDays_arr[i];
+    //meeting times 
     
+        meetingDays.forEach(function(element){
         
-    //     let days = newmeeting.split(" ")[0];
-    //     let startTime = newmeeting.split(" From</b>  ")[1]//.substring(8);
-    //     let endTime = newmeeting.split(" <b>to</b> ")[2]//.substring(8);
+        let meetingDay =  element.split(' ')[0];
+        let meetingStartTime = element.split(' ')[3]+' '+element.split(' ')[4];
+        let meetingEndTime = element.split(' ')[6]+' '+element.split(' ')[7];
+        let meetingType = element.split(' ')[10]
+                 
         
-    //     // allMeetings.push(meetingDetails)
         
-    //      }
+        allMeetings.push({
+            meetingName: aa_locationName, 
+            locationName: aa_addressName,
+            address: aa_address,
+            // zipCode: aa_zipCode,
+            wheelChairAccess: aa_wheelChairAccess, 
+            day: meetingDay, 
+            startTime: meetingStartTime, 
+            endTime: meetingEndTime,
+            type: meetingType
+        })
+        return allMeetings 
+        })
         
-
-    //     // console.log (meetingDetails)
-          
-    
-    
-
-    
-    
-    
-//     // log trs
-//     function logTrs (tr) {
-//         console.log(tr)
-//         console.log('*************')
-//         // console.log(meetings)
-//         console.log('*************')
-//         meetings.push(tr)
-//         // console.log(meetings.length)
+        console.log (allMeetings)
+    };
+       
         
-//     }    
-     }
-//  parseZones(content)
+        // fs.writeFileSync("addressData", JSON.stringify(allMeetings))
